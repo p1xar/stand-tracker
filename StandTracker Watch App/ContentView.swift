6e71fdd37c320e48277b78c833b2c9d89d0a35a8
p1xar <melnichukr@ya.ru>
2025-01-03 15:22:32 +0100

    Initial Commit

---
+++ StandTracker Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StandTracker Watch App
+//
+//  Created by Ruslan Melnichuk on 3/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("TEST")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StandTracker Watch App/StandTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandTrackerApp.swift
+//  StandTracker Watch App
+//
+//  Created by Ruslan Melnichuk on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct StandTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
